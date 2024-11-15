a1b122068f7c6db00fcefb39c89e1ea1b5774a62
tim3124 <94644510+tim3124@users.noreply.github.com>
2024-11-16 00:10:56 +0100

    Suprression des gestions d'erreurs

--- 1_Calcul_EDH.py
+++ 1_Calcul_EDH.py
@@ -5,7 +5,6 @@ import config_stock
 
 # Connexion à la base de données Analyse Stock Alluvial 
 def connect_bdd():
-    try:
         conn = psycopg2.connect(
             dbname=config_stock.DB_NAME,
             user=config_stock.DB_USER,
@@ -14,42 +13,25 @@ def connect_bdd():
             port=config_stock.DB_PORT
     )
 
-        print("Connexion à la base de données réussie")
-    except Exception as e:
-        print("Echec de la connexion à la Base de Données")
-        print(e)
-        exit(1)
-        
+        print("Connexion à la base de données...")
         conn.close()
     
-    try: 
     # Requête pour récupérer les données des tables 'BA_2021' et 'BA_2018' dans le schéma 'ba'
         query_table1 = "SELECT * FROM ba.bande_active_fluviale_2021_gdp"
         query_table2 = "SELECT * FROM ba.bande_active_fluviale_2018_gdp_vf"
 
-        print ("Requête réussie")
-    except Exception as e2 : 
-        print ("Voici une requête qui renvoie une erreur !")
-        print (e2)
-        exit(1)
-    try : 
+        print ("Requête réalisée")
         gdf_table1 = gpd.read_postgis(query_table1, conn, geom_col='geom')
         gdf_table2 = gpd.read_postgis(query_table2, conn, geom_col='geom')
 
         print ("Géométrie récuperée !!")
-    except Exception as e3 : 
         print ("Géométrie non récuperée :(")
-        print (e3)
-        exit(1)
-    return (gdf_table1, gdf_table2)
+        return (gdf_table1, gdf_table2)
 
 def traitement_spatiaux(gdf_table1, gdf_table2):
-    try:
-        merged_gdf = gdf_table1._append(gdf_table2, ignore_index=True)
-        dissolved_gdf = merged_gdf.dissolve()
-    except Exception as e:
-        exit(1)
-
+    merged_gdf = gdf_table1._append(gdf_table2, ignore_index=True)
+    dissolved_gdf = merged_gdf.dissolve()
+    
     #Fusion et regroupement des bandes actives
     merged_gdf = gdf_table1._append(gdf_table2, ignore_index=True)
     dissolved_gdf = merged_gdf.dissolve()
@@ -73,7 +55,6 @@ def traitement_spatiaux(gdf_table1, gdf_table2):
     simplified_gdf.to_file(output_file, driver='ESRI Shapefile')
     print ("Création de l'Enveloppe de Divagation Historique crée ! ")
 
-    exit(1)
     return dissolved_gdf, buffered_gdf, simplified_gdf, output_file
 
 gdf_table1, gdf_table2 = connect_bdd()
